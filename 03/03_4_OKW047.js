756f90b10318c52aed251e8af1bfefe0b65426c1
m_makino <m_makino@okwave.co.jp>
2016-10-24 19:03:42 +0900

    回答を追加

--- 03/03_4_OKW047.js
+++ 03/03_4_OKW047.js
@@ -1,11 +1,80 @@
 /**
  * Created by makinomasashi on 16/10/23.
  */
-var d1 = dhash('a891d05d006f7e7e29bd266f1a3314883ffb556fbf73caac6c4604d514b6944d')
-var d2 = dhash('6c4fb822beda2eb40bf212894711504b3cdca2af01e1fac469426feee477fa6d')
-var d3 = dhash('441582cfdb81dae14396578f239d35ad1419db7aaf46d056bf6738194414d01f')
-var d4 = dhash('1df8ee43477e43bf583fc0012fe7cbb22cb6a7b6223e98ec7c054bb7a20b89f3')
-var d5 = dhash('164577b2332bded00b27fd7943003b3036329de9d3bf1ee33f02beca0773c967')
-var d6 = dhash('210e8b96b3ce9d65ea9bf51b4c890f464fd81eb03e3648412469600b29d63045')
+var Readline = require('readline')
+var Hash     = require('yours-bitcoin/lib/hash')
 
-console.log(d1)
+// 標準入力を受け取る
+var rl = Readline.createInterface({
+  'input': process.stdin,
+  'output': {}
+});
+
+// 1行ずつ配列に入れて値を取得する
+var index = 0
+var dataArray = []
+rl.on('line', function (line) {
+  // 入力文字列から改行を削除
+  var input = line.replace(/\r?\n/g,"")
+
+  // 配列に追加
+  dataArray.push(input)
+});
+
+// すべての行の読み込みが終わった後にメインの処理を実行
+rl.on('close', function() {
+  // 配列の要素数が奇数だった場合最後の値を複製
+  var a = arrayToEvenNumber(dataArray)
+
+  // 配列の要素をLEに変換
+  for( var i = 0; i < a.length; i++ ) {
+    a[i] = reverseEndian(a[i])
+  }
+
+  // マークルツリーを計算する
+  var hash1 = ''
+  var hash2 = ''
+  var r     = []
+  while( a.length > 0 ) {
+    hash1 = a[0]
+    hash2 = a[1]
+    r.push(Hash.sha256Sha256(Buffer(hash1 + hash2, 'hex')).toString('hex'))
+    a.splice(0, 2)
+    if( a.length == 0 && r.length > 1 ) {
+      a = arrayToEvenNumber(r)
+      r = []
+    }
+  }
+
+  // リトルエンディアンにしてマークル・ルートを出力
+  process.stdout.write(reverseEndian(r[0].toString('hex')));
+});
+
+/**
+ * 配列が奇数の場合に一番最後の要素をコピーして偶数にする
+ */
+function arrayToEvenNumber(array) {
+  if( array.length % 2 != 0 ) {
+    array.push(array[array.length -1])
+  }
+  return array
+}
+
+/**
+ * エンディアンを逆にする
+ */
+function reverseEndian( hex ) {
+  // 桁数が2の倍数ではない場合、頭を0で埋める
+  while( hex.length % 2 != 0 ) {
+    hex = "0" + hex
+  }
+
+  var r = ""
+  var a = hex.match(/[\s\S]{1,2}/g);
+  a.reverse()
+
+  for(var i = 0; i < a.length; i++) {
+    r = r + a[i]
+  }
+  return r
+}
