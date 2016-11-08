8cb1eff6ace748c9d951a65d3c9fb8e58761d43f
m_makino <m_makino@okwave.co.jp>
2016-11-08 13:18:09 +0900

    課題05完了

--- 05/05_2_OKW047.js
+++ 05/05_2_OKW047.js
@@ -1,9 +1,22 @@
 /**
  * Created by makinomasashi on 16/11/06.
  */
-let mn = require('bitcore-mnemonic');
+var Bip39    = require('yours-bitcoin/lib/bip-39');
+var Bip32    = require('yours-bitcoin/lib/bip-32');
 //var data = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-var data = require('fs').readFileSync(process.stdin.fd).toString().split('\r\n');
-var code = new mn(data[0]);
-var xpriv = code.toHDPrivateKey(data[1]);
-console.log(xpriv.toString());
+var dataArray = require('fs').readFileSync(process.stdin.fd).toString().split('\n');
+var nemo = dataArray.shift();
+var pass = dataArray.shift();
+
+// BIP39のオブジェクトを作成
+var b39 = Bip39.fromString(nemo);
+
+// ニーモニックとパスフレーズからシードを作成
+b39.mnemonic2Seed(pass);
+var seed = b39.seed;
+
+// SeedからBIP32のオブジェクトを作成
+var b32 = Bip32.fromSeed(seed);
+
+// マスタ鍵を出力
+console.log(b32.toString());
--- 05/05_3_OKW047.js
+++ 05/05_3_OKW047.js
@@ -1,15 +1,28 @@
 /**
  * Created by makinomasashi on 16/11/07.
  */
-var data = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-var mPrvKey = data.shift();
-var path = data.shift();
-var crypto = require('crypto');
-var hash = crypto.HmacSHA256(mPrvKey, path);
-console.log(hash);
-//var prvKey
-//var pubKey
-//var address
-//console.log(bs58check.encode(prvKey));
-//console.log(bs58check.encode(pubKey));
-//console.log(bs58check.encode(address));
+var Address  = require('yours-bitcoin/lib/address');
+var Bip32    = require('yours-bitcoin/lib/bip-32');
+//var data = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+var dataArray = require('fs').readFileSync(process.stdin.fd).toString().split('\n');
+var mPrvKey = dataArray.shift();
+var path = dataArray.shift();
+
+// BIP32のオブジェクトを作成
+var b32 = Bip32.fromString(mPrvKey);
+
+// 派生パスを指定
+path = path.replace(/’/g, "'");
+var derive = b32.derive(path);
+
+// 拡張秘密鍵と拡張公開鍵を取得
+var xprv = derive.toString();
+var xpub = derive.toPublic().toString();
+
+// ビットコインアドレスのオブジェクトを生成
+var address = Address.fromPubKey(derive.pubKey);
+
+// 拡張秘密鍵と拡張公開鍵とビットコインアドレスを出力
+console.log(xprv);
+console.log(xpub);
+console.log(address.toString());
